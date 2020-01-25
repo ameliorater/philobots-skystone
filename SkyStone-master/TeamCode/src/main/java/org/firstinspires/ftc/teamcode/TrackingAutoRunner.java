544230f7503fd854599cc13997e58369eb5b47c2
amelialeveille <amelialeveille@gmail.com>
2020-01-25 10:20:24 -0600

    clean version v2

---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrackingAutoRunner.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Tracking Test Blue", group="Linear Opmode")
+public class TrackingAutoRunner extends TrackingAutoTest {
+
+    public TrackingAutoRunner() {
+        super(true);
+    }
+}
---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrackingAutoRunner2.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Tracking Test Red", group="Linear Opmode")
+public class TrackingAutoRunner2 extends TrackingAutoTest {
+
+    public TrackingAutoRunner2() {
+        super(false);
+    }
+}
